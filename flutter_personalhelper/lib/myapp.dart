6dbd449ed6486a282a434e407ddb16c473f91ac9
Isaac-Leonel <isaacleonel72@gmail.com>
2022-05-13 11:48:25 -0300

    teste

---
+++ flutter_personalhelper/lib/myapp.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
