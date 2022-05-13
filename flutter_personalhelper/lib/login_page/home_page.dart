cccd940e3f160abc02c52d9450d68d4346dda9e8
Samuel Lamera <lamerasamuel@gmail.com>
2022-05-13 14:22:43 -0300

    aaa

--- flutter_personalhelper/lib/login_page/home_page.dart
+++ flutter_personalhelper/lib/login_page/home_page.dart
@@ -1,17 +1,46 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+import '../components/inputcad.dart';
+
+class Login extends StatelessWidget {
+  const Login({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
-      ),
+      backgroundColor: Color(0xFF00261d),
       body: Center(
-        child: Text('Personal Helper'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: Image.asset(
+                'assets/img/logoimg.png',
+                fit: BoxFit.fitWidth,
+              ),
+            ),
+            const Text(
+              'CADASTRO',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 25),
+            ),
+            const Text(
+              'IDOSO',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.normal,
+                  fontSize: 18),
+            ),
+            const InputCad(
+              inputname: 'Email',
+            )
+          ],
+        ),
       ),
     );
   }
