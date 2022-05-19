bceb4971a43b70d914128955ce0399df590aaf4c
Isaac-Leonel <isaacleonel72@gmail.com>
2022-05-19 16:18:15 -0300

    CadastroPost

--- flutter_personalhelper/lib/singup_page/singup_controller.dart
+++ flutter_personalhelper/lib/singup_page/singup_controller.dart
@@ -1,4 +1,25 @@
-import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
+import 'package:personal_helper/models/idoso.dart';
 
-class SingUpController {}
+class SingUpController {
+  final Idoso idoso;
+  String urlIdoso = 'https://localhost:3306/cadastrarIdoso';
+
+  SingUpController(this.idoso);
+
+  postIdoso() async {
+    try {
+      var response = await http.post(Uri.parse(urlIdoso), body: {
+        "nome": idoso.nome,
+        "cpf": idoso.cpf,
+        "nascimento": idoso.nascimento,
+        "email": idoso.email,
+        "senha": idoso.senha,
+        "rg": idoso.rg
+      });
+    } catch (e) {
+      // ignore: avoid_print
+      print(e);
+    }
+  }
+}
