30b4c14c8728127d9b5a5ac727ff74b54975ef47
Samuel Lamera <lamerasamuel@gmail.com>
2022-05-25 16:55:23 -0300

    likando as telas

---
+++ flutter_personalhelper/lib/components/inputcads.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class InputCadLarg extends StatelessWidget {
+  const InputCadLarg({Key? key, required this.inputname}) : super(key: key);
+
+  final String inputname;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.only(left: 15),
+          child: Text(
+            inputname,
+            style: const TextStyle(
+                color: Colors.white, fontWeight: FontWeight.w300, fontSize: 18),
+          ),
+        ),
+        SizedBox(
+          height: 90,
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            child: TextFormField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                ),
+                filled: true,
+                fillColor: Colors.white,
+              ),
+              validator: (String? value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
--- flutter_personalhelper/lib/dashboard/edita_remedio.dart
+++ flutter_personalhelper/lib/dashboard/edita_remedio.dart
@@ -6,26 +6,107 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
-import '../components/inputcad.dart';
+import '../components/inputcads.dart';
+import '../components/inputcadmin.dart';
+import '../components/inputcadminr.dart';
 
 class Edita extends StatelessWidget {
   const Edita({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF237C6A),
+      backgroundColor: const Color(0xFF237C6A),
       body: SingleChildScrollView(
         child: Column(
           children: [
             Container(
               color: const Color(0xFF00261d),
-              height: 200,
+              height: 280,
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 350, top: 30),
+                    child: SizedBox(
+                        child: Image.asset(
+                      'assets/img/config.png',
+                    )),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(),
+                    child: SizedBox(
+                      child: Image.asset(
+                        'assets/img/arrow.png',
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 350),
+                    child: SizedBox(
+                      child: Image.asset(
+                        'assets/img/senhor.png',
+                        fit: BoxFit.fitHeight,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 134, top: 5, right: 134),
+                    child: SizedBox(
+                      child: Image.asset(
+                        'assets/img/remedio.png',
+                        fit: BoxFit.fitHeight,
+                      ),
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(
+                        left: 134, top: 10, right: 134, bottom: 40),
+                    child: Text(
+                      'Paracetamol',
+                      style: const TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 25),
+                    ),
+                  )
+                ],
+              ),
             ),
             Container(
-              color: Color.fromARGB(255, 88, 153, 138),
-              height: 200,
+              child: Column(
+                children: [
+                  const InputCadLarg(
+                    inputname: 'Descricao',
+                  ),
+                  const InputCadmin(
+                    inputname: 'Quantidade',
+                  ),
+                  const InputCadminr(
+                    inputname: 'Vencimento',
+                  ),
+                  const InputCadmin(
+                    inputname:
+                        'Horario de                                                        administração',
+                  ),
+                  const InputCadminr(
+                    inputname: 'Horario de administração 2',
+                  ),
+                  const InputCadmin(
+                    inputname: 'função',
+                  ),
+                  const InputCadminr(
+                    inputname: 'Tarja',
+                  ),
+                ],
+              ),
             ),
-            Text('Insira embaixo')
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: FloatingActionButton(
+                  child: Icon(Icons.task_alt),
+                  backgroundColor: const Color(0xFF32A18A),
+                  onPressed: () {}),
+            ),
           ],
         ),
       ),
