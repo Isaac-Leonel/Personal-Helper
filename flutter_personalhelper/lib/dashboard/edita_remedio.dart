818434a434b2af842eaa4991a58814a1919090a9
Isaac-Leonel <93339716+Isaac-Leonel@users.noreply.github.com>
2022-05-25 14:21:46 -0300

    Container

--- flutter_personalhelper/lib/dashboard/edita_remedio.dart
+++ flutter_personalhelper/lib/dashboard/edita_remedio.dart
@@ -15,9 +15,18 @@ class Edita extends StatelessWidget {
     return Scaffold(
       backgroundColor: Color(0xFF237C6A),
       body: SingleChildScrollView(
-        child: Container(
-          color: const Color(0xFF00261d),
-          height: 200,
+        child: Column(
+          children: [
+            Container(
+              color: const Color(0xFF00261d),
+              height: 200,
+            ),
+            Container(
+              color: Color.fromARGB(255, 88, 153, 138),
+              height: 200,
+            ),
+            Text('Insira embaixo')
+          ],
         ),
       ),
     );
