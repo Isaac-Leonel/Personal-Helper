1b3ac172e1fc8189e96755a9ea7d91a84c9cd71c
Bruno Almeida <almeidabruno19@outlook.com>
2022-06-06 21:13:48 -0300

    cartao

---
+++ flutter_personalhelper/lib/components/moduleCardCartao.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class ModuleCardCartao extends StatelessWidget {
+  const ModuleCardCartao(
+      {Key? key, required this.iconModule, required this.textModule})
+      : super(key: key);
+
+  final String iconModule;
+  final String textModule;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).pushNamed('/cartaoSaude');
+      },
+      child: Container(
+        width: 150,
+        height: 150,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: const Color(0xFF32A18A),
+          boxShadow: [
+            BoxShadow(color: const Color(0xFF32A18A), spreadRadius: 3),
+          ],
+        ),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                    width: 100, height: 100, child: Image.asset(iconModule)),
+                Text(
+                  textModule,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
